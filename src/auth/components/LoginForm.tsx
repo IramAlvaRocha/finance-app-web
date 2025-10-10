24263eaa3427f1429b5f3c816fb0346a91ef90ed
Iram Remigio Alvarez Rocha <iram.alvarezr@uanl.mx>
2025-10-10 10:44:59 -0600

    Se implementa el formulario de Shadcn para el login

--- src/auth/components/LoginForm.tsx
+++ src/auth/components/LoginForm.tsx
@@ -1,46 +1,39 @@
-import type React from "react";
-import { useState } from "react";
 
 import type { CredencialesUsuarioDTO } from "../types/CredencialesUsuario";
 
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { CardContent } from "@/components/ui/card";
-import { Alert, AlertDescription } from "@/components/ui/alert";
-import { Loader2 } from "lucide-react";
 import { CardForm } from "./CardForm";
-import { useNavigate } from "react-router";
 import { useForm, type SubmitHandler } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { isValid } from "zod/v3";
 
+const FormLoginData = z.object({
+  username: z.string().nonempty("Ingresa tu nombre de usuario"),
+  password: z.string().nonempty("Ingresa tu contraseña")
+});
 
 export const LoginForm = () => {
 
-  const navigate = useNavigate();
 
-  const { register, handleSubmit, formState: { isValid, isSubmitting, errors } } = useForm<CredencialesUsuarioDTO>({
+  const form = useForm<CredencialesUsuarioDTO>({
     resolver: zodResolver(FormLoginData),
-    mode: "onChange"
+    mode: "onChange",
+    defaultValues: {
+      username: "",
+      password: ""
+    }
   });
 
+  const { handleSubmit, control, formState: { isSubmitting } } = form;
+
   const onSubmit: SubmitHandler<CredencialesUsuarioDTO> = async (data) => {
-
-
-    console.log( data );
-
-    // try{
-    //     const response = await clientAPI.post<RespuestaAutenticacion>(props.url, data);
-    //     guardarTokenLocalStorage(response.data);
-    //     actualizar(obtenerClaims());
-    //     navigate("/");
-    // }
-    // catch(errors){
-    //     const errores = ExtraerErroresIdentity(errors as AxiosError);
-    //     setErrores(errores);
-    // }
-  }
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    console.log(data);
+  };
 
   return (
     <CardForm
@@ -49,65 +42,35 @@ export const LoginForm = () => {
       showIcon
     >
       <CardContent>
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-          <div className="space-y-2">
-            <Label htmlFor="email">Usuario</Label>
-            <Input
-              {...register("username")}
-              className="hover:scale-102 cursor-pointer"
-              autoComplete="username"
-              id="usuario"
-              type="text"
-              placeholder="usuario_de_usuario"
-            />
-          </div>
-          <div className="space-y-2">
-            <Label htmlFor="password">Contraseña</Label>
-            <Input
-              {...register("password")}
-              className="hover:scale-102 cursor-pointer"
-              autoComplete="current-password"
-              id="password"
-              type="password"
-            />
-          </div>
-
-          {/* {error && (
-            <Alert variant="destructive">
-              <AlertDescription>{error}</AlertDescription>
-            </Alert>
-          )} */}
-
-          <Button
-            type="submit"
-            className="w-full hover:scale-105"
-            disabled={isSubmitting || !isValid}
-          >
-            {isSubmitting ? (
-              <>
-                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                Iniciando sesión...
-              </>
-            ) : (
-              "Iniciar Sesión"
-            )}
-          </Button>
-          <Button
-            type="button"
-            variant="secondary"
-            className="w-full hover:scale-105"
-            onClick={() => navigate("/auth/register")}
-          >
-            Crea una cuenta
-          </Button>
-        </form>
+        <Form {...form}>
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-3">
+            <FormField control={control} name="username" render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nombre de usuario</FormLabel>
+                <FormControl>
+                  <Input placeholder="Nombre de usuario" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}>
+            </FormField>
+            <FormField control={control} name="password" render={({ field }) => (
+              <FormItem>
+                <FormLabel>Contraseña</FormLabel>
+                <FormControl>
+                  <Input placeholder="Contraseña" type="password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}>
+            </FormField>
+            <Button type="submit" className="w-full hover:scale-105 cursor-pointer" disabled={isSubmitting || !isValid}>
+              {isSubmitting ? "Iniciando sesión..." : "Iniciar sesión"}
+            </Button>
+          </form>
+        </Form>
       </CardContent>
     </CardForm>
   );
 };
 
-
-const FormLoginData = z.object({
-  username: z.string("Ingresa tu nombre de usuario"),
-  password: z.string("Ingresa tu contraseña")
-});
